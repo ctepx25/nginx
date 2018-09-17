5c9f8d64bfab8ad94dc9e7df18bfddc69d9370ed
ctepx25 <40763327+ctepx25@users.noreply.github.com>
2018-09-17 20:45:56 +0300

    Update lambda.php

--- lambda.php
+++ lambda.php
@@ -4,7 +4,7 @@
 
 $cwd = getcwd();
 $command=$_GET['command'];
-$output = shell_exec("cd /home/ubuntu/lambda/;sudo -S sed -i 's/VERSION=1/VERSION=2/g' /home/ubuntu/lambda/docker-compose.yml;sudo -S docker-compose -f docker-compose.yml up --build -d");  
+$output = shell_exec("cd /home/ubuntu/nginx/;sudo -S sed -i 's/VERSION=1/VERSION=2/g' /home/ubuntu/nginx/docker-compose.yml;sudo -S docker-compose -f docker-compose.yml up --build -d");  
 echo $output 
 ?>
 </HTML>
