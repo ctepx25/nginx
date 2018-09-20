1b66115ff0f6147142b334a2dd6fdcf11fbc631b
ctepx25 <40763327+ctepx25@users.noreply.github.com>
2018-09-20 12:46:01 +0300

    Update and rename lambda_function.py to worker_function.py

--- lambda_function.py
+++
@@ -1,4 +0,0 @@
-def lambda_handler(event, context):
-    import subprocess
-    result = subprocess.call("curl -I http://HOST/lambda.php?command=deploy", shell=True)
-    return result
---
+++ worker_function.py
@@ -0,0 +1,32 @@
+import boto3
+import paramiko, json
+def worker_handler(event, context):
+
+    s3_client = boto3.client('s3')
+    s3_client.download_file('ctepx-lambda-nginx','id_rsa', '/tmp/id_rsa')
+
+    k = paramiko.RSAKey.from_private_key_file("/tmp/id_rsa")
+    c = paramiko.SSHClient()
+    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    host = event['Records'][0]['body'].split(',')[0]
+    action = event['Records'][0]['body'].split(',')[1]
+    print "Connecting to " + host
+    c.connect( hostname = host, username = "root", pkey = k )
+    print "Connected to " + host
+
+    command = 'echo '+action+' >> /tmp/log.txt'
+    command2 = 'sed \'s/VERSION=1/VERSION=2/g\' /home/ubuntu/nginx/docker-compose.yml > /tmp/docker-compose.yml;docker-compose -f /tmp/docker-compose.yml up --build -d'
+    
+    print "Executing {}".format(command)
+    print "Executing {}".format(command2)
+    stdin , stdout, stderr = c.exec_command(command)
+    stdin , stdout, stderr = c.exec_command(command2)
+    print stdout.read()
+    print stderr.read()
+
+    return
+    {
+        "statusCode": 200,
+        "body": json.dumps('Hello from Lambda!')
+    }
